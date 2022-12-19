97b6b54df26029b0db3685937348b182ea78abb1
claudiogodoy99 <cavalcantigodoy@gmail.com>
2022-12-19 15:02:34 -0300

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { MsalBroadcastService } from '@azure/msal-angular/msal.broadcast.service';
+import { MsalGuard } from '@azure/msal-angular/msal.guard';
+import { AccountInfo } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'auth';
+
+  access_token = ''
+
+  constructor(private authService: MsalService) { }
+
+  ngOnInit(): void {
+
+    //depara
+    //mf 1 - 1234,
+    //mf 2 - 12345,
+    
+    this.authService.ssoSilent({
+      loginHint: 'claudiogodoy@microsoft.com',
+      scopes: ['']
+    }).subscribe(x => {
+      this.authService.acquireTokenSilent({
+        scopes: [''],
+        account: x.account as AccountInfo
+      }).subscribe(y => {
+        console.log(y)
+        this.access_token = y.accessToken;
+      });
+      
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MsalGuard, MsalModule } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: '', 
+        authority: 'https://login.microsoftonline.com//',
+        redirectUri: 'http://localhost:4200'
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: false, 
+      }
+    }),
+    {
+      interactionType: InteractionType.Popup, // MSAL Guard Configuration
+      authRequest: {
+        scopes: ['https://admin.services.crm.dynamics.com/user_impersonation']
+      }
+    },  
+    {
+      interactionType: InteractionType.Popup, // MSAL Interceptor Configuration
+      protectedResourceMap: new Map([ 
+          ['https://api.com', ['https://admin.services.crm.dynamics.com/user_impersonation']]
+      ])
+    })
   ],
-  providers: [],
+  providers: [
+    MsalGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
